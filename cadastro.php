84671071f6ecc6b328e6d9ca78c3cf82be4a2bb3
Márcio Miranda <ti.marcio.miranda@drummond.com.br>
2020-03-16 16:36:34 -0300

    Arquivos atualizados

--- cadastro.php
+++ cadastro.php
@@ -5,7 +5,7 @@ use App\Alura\Contato;
 
 require 'autoload.php';
 
-$usuario = new Usuario($_POST['nome'], $_POST['senha']);
+$usuario = new Usuario($_POST['nome'], $_POST['senha'], $_POST['genero']);
 $contato = new Contato($_POST['email'], $_POST['endereco'], $_POST['cep'], $_POST['telefone']);
 
 ?>
@@ -22,15 +22,15 @@ $contato = new Contato($_POST['email'], $_POST['endereco'], $_POST['cep'], $_POS
 
 <div class="mx-5 my-5">
 <h1>Cadastro feito com sucesso.</h1>
-<p>Seguem os dados de sua conta:</p>
+<p><?php echo htmlspecialchars($usuario->getTratamento()); ?>, seguem os dados de sua conta:</p>
 <ul class="list-group">
-    <li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?></li class="list-group-item">
-    <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?></li class="list-group-item">
-    <li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li class="list-group-item">
-    <li class="list-group-item">Senha: <?php echo $usuario->getSenha() ?></li class="list-group-item">
-    <li class="list-group-item">Telefone: <?php echo $contato->getTelefone() ?></li class="list-group-item">
-    <li class="list-group-item">Email: <?php echo $contato->getEmail(); ?></li class="list-group-item">
-    <li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep() ?></li class="list-group-item">
+    <li class="list-group-item">Primeiro nome: <?php echo htmlspecialchars($usuario->getNome()); ?></li class="list-group-item">
+    <li class="list-group-item">Sobrenome: <?php echo htmlspecialchars($usuario->getSobrenome()); ?></li class="list-group-item">
+    <li class="list-group-item">Usuário: <?php echo htmlspecialchars($contato->getUsuario()); ?></li class="list-group-item">
+    <li class="list-group-item">Senha: <?php echo htmlspecialchars($usuario->getSenha()) ?></li class="list-group-item">
+    <li class="list-group-item">Telefone: <?php echo htmlspecialchars($contato->getTelefone()) ?></li class="list-group-item">
+    <li class="list-group-item">Email: <?php echo htmlspecialchars($contato->getEmail()); ?></li class="list-group-item">
+    <li class="list-group-item">Endereço: <?php echo htmlspecialchars($contato->getEnderecoCep()) ?></li class="list-group-item">
 </ul>
 </div>
 </body>
